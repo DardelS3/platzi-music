9504b025f58675f55e70fc668b5604d7ee7965fa
steve <stevebeteta@gmail.com>
2019-03-17 00:57:13 -0600

    commit inicial

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default {
+  appName: 'Platzi-Music',
+  apiKey: 'f73588b4b5d7b0f46cf4fc03323ff71c',
+  secret: 'ada8d75d6a876058333de708a725abd5',
+  registeredTo: 'DardelS3'
+}
---
+++ src/components/Artist.vue
@@ -0,0 +1,27 @@
+<template>
+  <li>
+    <h2>
+      <a :href="artist.url" target="_blank">{{artist.name}}
+        <img :src="artist.image[2]['#text']" alt="">
+      </a>
+    </h2>
+  </li>
+</template>
+
+<script>
+  export default {
+    name: 'artist',
+    props: ['artist'],
+    data() {
+      return {
+        artists: []
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+li.artist
+  display block
+  margin 10px 0
+</style>
